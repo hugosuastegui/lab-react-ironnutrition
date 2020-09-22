ef49809a3d76e87c951f264cc37eb83f3430d397
Hugo Suastegui <hugo.suastegui.pineda@gamil.com>
2020-09-21 23:32:09 -0500

    Bug fix

--- src/App.js
+++ src/App.js
@@ -1,96 +1,68 @@
-import React, {useState, useEffect} from 'react';
-import './App.css';
-import foods from './foods.json'
+import React, {useState} from 'react';
 import FoodBox from './components/FoodBox'
+import meals from './foods.json'
 
 
 function App() {
-  const [food, setfood] = useState(foods)
-  const [query, setquery] = useState('')
-  const [results, setresults] = useState()
-  const [form, setform] = useState(false)
-  const [name, setname] = useState('Turkey')
-  const [image, setimage] = useState('https://upload.wikimedia.org/wikipedia/commons/4/4d/Wild_turkey_eastern_us.jpg')
-  const [calories, setcalories] = useState(100)
-    
-  useEffect(() => {
-    setfood(foods)
-  }, [])
-  
-  async function searchQuery(value){
-    await setquery(value)
-  }
-  
-  useEffect(() => {
-    const search = food.filter(item => {
-      return item.name.toLowerCase().includes(query.toLowerCase())
-    })
-    setresults(search)
-    console.log(results)
-  }, [query])
-  
-  const handleFormSubmit = (e) => {
-    e.preventDefault()
-    setfood([...food, {name, image, calories}])
-    setname('')
-    setimage('')
-    setcalories(0)
-}
-
-  return results ? (
-    <div className="container">
-      <h1 class="title">IronNutrition</h1>
-      <div className="column is-desktop">
-          <div className='column'>
-            <button style={{float: 'left', margin: '1rem'}} onClick={() => setform(!form)}>Add Food</button>
-            {form ? (
-              <form onSubmit={handleFormSubmit}>
-              <label>Name:</label>
-              <input
-                type='text'
-                name='name'
-                value={name}
-                onChange={e => setname(e.target.value)}
-              />
-              <label>Director:</label>
-              <input
-                type='text'
-                name='image'
-                value={image}
-                onChange={e => setimage(e.target.value)}
-              />
-
-              <label>calories:</label>
-              <input
-                type='text'
-                name='calories'
-                value={calories}
-                onChange={e => setcalories(e.target.value)}
-              />
-              <button>Submit</button>
-            </form>
-            ) : ''}
-            <input
-              className="input search-bar"
-              type='text'
-              name='query'
-              placeholder="Search"
-              value={query}
-              onChange={e => searchQuery(e.target.value)}
-            />
-            </div>
-            {results.map((el, ind) => (
-              <FoodBox key={ind} food={el}></FoodBox>
-            ))}
-          </div>
-
-          <div className='column'>
-              <h2>Tabla Nutrimental</h2>
-          </div>
-
-      </div>
-
-  ) : <h1>"Loading"</h1>
+  const [foods] = useState(meals)
+  return(
+    <FoodBox foods={foods}></FoodBox>
+  )
 }
 
 export default App;
+
+// return results ? (
+//   <div className="container">
+//     <h1 class="title">IronNutrition</h1>
+//     <button style={{float: 'left', margin: '1rem'}} onClick={() => setform(!form)}>Add Food</button>
+//     {form ? (
+//       <form onSubmit={handleFormSubmit}>
+//       <label>Name:</label>
+//       <input
+//         type='text'
+//         name='name'
+//         value={name}
+//         onChange={e => setname(e.target.value)}
+//       />
+//       <label>Director:</label>
+//       <input
+//         type='text'
+//         name='image'
+//         value={image}
+//         onChange={e => setimage(e.target.value)}
+//       />
+
+//       <label>calories:</label>
+//       <input
+//         type='text'
+//         name='calories'
+//         value={calories}
+//         onChange={e => setcalories(e.target.value)}
+//       />
+//       <button>Submit</button>
+//     </form>
+//     ) : ''}
+//     <input
+//       className="input search-bar"
+//       type='text'
+//       name='query'
+//       placeholder="Search"
+//       value={query}
+//       onChange={e => searchQuery(e.target.value)}
+//     />
+//     <div className="columns">
+//         <div className='column'>
+//           {results.map((el, ind) => (
+//             <FoodBox key={ind} food={el}></FoodBox>
+//           ))}
+//           </div>
+//         <div className='column content'>
+//             <h2>Tabla Nutrimental</h2>
+//         </div>
+//       </div>
+
+
+//     </div>
+
+// ) : <h1>"Loading"</h1>
--- src/components/FoodBox.js
+++ src/components/FoodBox.js
@@ -1,36 +1,133 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react';
+// import {Form, Button, InputNumber} from 'antd'
 
-function FoodBox({food}) {
-    return (
-            <div className="box">
-                <article className="media">
-                    <div className="media-left">
-                    <figure className="image is-64x64">
-                        <img src={food.image} alt="FoodImage"/>
-                    </figure>
-                    </div>
-                    <div className="media-content">
-                    <div className="content">
-                        <p>
-                        <strong>{food.name}</strong> <br />
-                        <small>{food.calories} cal</small>
-                        </p>
-                    </div>
-                    </div>
-                    <div className="media-right">
-                    <div className="field has-addons">
-                        <div className="control">
-                        <input className="input" type="number" />
+function FoodBox({foods}) {
+    const [food, setfood] = useState(foods)
+    const [results, setresults] = useState()
+    const [query, setquery] = useState('')
+    const [toggleForm, settoggleForm] = useState(false)
+    const [name, setname] = useState('Turkey')
+    const [image, setimage] = useState('https://upload.wikimedia.org/wikipedia/commons/4/4d/Wild_turkey_eastern_us.jpg')
+    const [calories, setcalories] = useState(100)
+    const [quantity, setquantity] = useState(0)
+    const [diet, setdiet] = useState([])
+
+    async function searchQuery(value){
+      await setquery(value)
+    }
+    
+    useEffect(() => {
+      const search = food.filter(item => {
+        return item.name.toLowerCase().includes(query.toLowerCase())
+      })
+      setresults(search)
+    }, [query])
+    
+    const handleFormSubmit = (e) => {
+      e.preventDefault()
+      setfood([...food, {name, image, calories}])
+      setname('')
+      setimage('')
+      setcalories(0)
+  }
+  
+  async function addToTable(index){
+    let tableItem = food[index]
+    tableItem.quantity = quantity
+    tableItem.onTable = true
+    const newArr = [...food].filter((el, ind) => ind !== index)
+    await newArr.splice(index, 0, tableItem)
+    setfood(newArr)
+    const onDiet = food.filter(el => el.onTable)
+    console.log(onDiet)
+    setdiet(onDiet)
+  }
+  
+
+    return results? (
+        <div className="container">
+        <h1 className="title">IronNutrition</h1>
+        <button style={{float: 'left', margin: '1rem'}} onClick={() => settoggleForm(!toggleForm)}>Add Food</button>
+        {toggleForm ? (
+          <form onSubmit={handleFormSubmit}>
+          <label>Name:</label>
+          <input
+            type='text'
+            name='name'
+            value={name}
+            onChange={e => setname(e.target.value)}
+          />
+          <label>Director:</label>
+          <input
+            type='text'
+            name='image'
+            value={image}
+            onChange={e => setimage(e.target.value)}
+          />
+  
+          <label>calories:</label>
+          <input
+            type='text'
+            name='calories'
+            value={calories}
+            onChange={e => setcalories(e.target.value)}
+          />
+          <button>Submit</button>
+        </form>
+        ) : ''}
+        <input
+          className="input search-bar"
+          type='text'
+          name='query'
+          placeholder="Search"
+          value={query}
+          onChange={e => searchQuery(e.target.value)}
+        />
+        <div className="columns">
+            <div className='column'>
+              {results.map((el, ind) => (
+                <div key={ind} className="box">
+                  <article className="media">
+                      <div className="media-left">
+                      <figure className="image is-64x64">
+                          <img src={el.image} alt="FoodImage"/>
+                      </figure>
+                      </div>
+                      <div className="media-content">
+                      <div className="content">
+                          <p>
+                          <strong>{el.name}</strong> <br />
+                          <small>{el.calories} cal</small>
+                          </p>
+                      </div>
+                      </div>
+                      <div className="media-right">
+                        <div className="field has-addons">
+                            <div className="control">
+                                <input type="number" className="input" onChange={e => setquantity(e.target.value)}/>
+                            </div>
+                            <div className="control">
+                                <button className="button is-info" onClick={() => addToTable(ind)}>+</button>
+                            </div>
                         </div>
-                        <div className="control">
-                        <button className="button is-info">
-                            +
-                        </button>
-                        </div>
-                    </div>
-                    </div>
-                </article>
+                      </div>
+                  </article>
+              </div>
+              ))}
             </div>
+            <div className='column content'>
+                <h3>Today's foods</h3>
+                <ul>
+                    {diet.map((el, ind) => (
+                        <li key={ind}>{el.quantity} - {el.name}</li>
+                    ))}
+                </ul>
+                <strong>Total calories on the table {diet.reduce((acc, curr) => acc + (curr.quantity * curr.calories),0)}</strong>
+            </div>
+          </div>
+        </div>
+    ) : (
+        <h1>Loading...</h1>
     )
 }
 
